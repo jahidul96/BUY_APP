adf4b77eefe9f0a03248315dd1c41592d419ae31
jahidul96 <jahidul59895@gmail.com>
2022-11-30 12:39:10 +0600

    some cahnges and context added

--- admin_client/App.js
+++ admin_client/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { KeyboardAvoidingView, Platform, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./app/screens/HomeScreen";
@@ -8,47 +8,53 @@ import Login from "./app/screens/Login";
 import LoggedComp from "./app/components/LoggedComp";
 import { useEffect, useState } from "react";
 import { getUserFromAsync } from "./app/utils/LocalStorage";
+import { UserProvider } from "./app/context/UserContext";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   const [user, setUser] = useState(null);
+
   useEffect(() => {
     getUserFromAsync()
       .then((value) => {
         setUser(value);
-        // console.log(value);
       })
       .catch((err) => console.log(err.message));
   }, []);
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
+    <UserProvider>
+      <KeyboardAvoidingView
+        style={styles.container}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
       >
-        {user ? (
-          <>
-            <Stack.Screen name="Home" component={HomeScreen} />
-          </>
-        ) : (
-          <>
-            <Stack.Screen name="LoggedComp" component={LoggedComp} />
-            <Stack.Screen name="Register" component={Register} />
-            <Stack.Screen name="Login" component={Login} />
-          </>
-        )}
-      </Stack.Navigator>
-    </NavigationContainer>
+        <NavigationContainer>
+          <Stack.Navigator
+            screenOptions={{
+              headerShown: false,
+            }}
+          >
+            {user ? (
+              <>
+                <Stack.Screen name="Home" component={HomeScreen} />
+              </>
+            ) : (
+              <>
+                <Stack.Screen name="LoggedComp" component={LoggedComp} />
+                <Stack.Screen name="Register" component={Register} />
+                <Stack.Screen name="Login" component={Login} />
+              </>
+            )}
+          </Stack.Navigator>
+        </NavigationContainer>
+      </KeyboardAvoidingView>
+    </UserProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
--- admin_client/app/context/UserContext.js
+++ admin_client/app/context/UserContext.js
@@ -3,10 +3,10 @@ import { createContext, useState } from "react";
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-  const [auth, setAuthUser] = useState(null);
+  const [authUser, setAuthUser] = useState(null);
 
   return (
-    <UserContext.Provider value={{ auth, setAuthUser }}>
+    <UserContext.Provider value={{ authUser, setAuthUser }}>
       {children}
     </UserContext.Provider>
   );
